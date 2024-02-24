52b4adfe96ef1ae4ca63875caa216e16fda2d0b1
antsz25 <l20211845@tectijuana.edu.mx>
2024-02-23 21:07:10 -0800

    Model juego fixed

--- backend/models/juego.model.js
+++ backend/models/juego.model.js
@@ -5,10 +5,7 @@ const juegoSchema = Joi.object({
     categoria: Joi.string().min(3).max(30).required(),
     precio: Joi.number().required(),
     stock: Joi.number().optional(),
-    imagen: Joi.object({
-        filename: Joi.string().min(3).max(30).required().without('.'),
-        extension: Joi.string().min(3).max(5).required().allow('png', 'jpg', 'jpeg'),
-    }),
+    imagen: Joi.string().min(3).max(30).required().pattern(/^([a-zA-Z0-9])+\.(jpg|png|jpeg)$/), // Solo acepta imagenes con extension jpg, png o jpeg
     urlNFT: Joi.string().min(3).max(30).optional(),
 });
 
