5ab03a1c66b3de35c8298f6e06b0a1f76e738482
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-23 22:48:08 -0800

    Fixed issues with const

--- backend/db/mongodb.js
+++ backend/db/mongodb.js
@@ -1,114 +1,121 @@
-require('dotenv').config();
-const {MongoClient} = require( 'mongodb' );
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const _url = process.env.MONGODB_URI;
 const _database = process.env.MONGODB_DATABASE_NAME;
 const client = new MongoClient(_url);
 
-async function dbConnectionWrapper (functionWrapped){
-    await client.connect();
-    dbCon = client.db(_database, { useUnifiedTopology: true })
-    result = await functionWrapped(dbCon)
-    await client.close();
+async function dbConnectionWrapper(functionWrapped) {
+  await client.connect();
+  dbCon = client.db(_database, { useUnifiedTopology: true });
+  const result = await functionWrapped(dbCon);
+  await client.close();
+  return result;
+}
+
+async function createUsuario(data) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const result = await dbCon.collection("users").insertOne(data);
     return result;
+  });
+  return result;
 }
 
-async function createUsuario(data){
-    
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const result = await dbCon.collection("users").insertOne(data)
-        return result
-    })
+async function getUsuarios() {
+  usuarios = await dbConnectionWrapper(async (dbCon) => {
+    const result = await dbCon.collection("users").find().toArray();
     return result;
+  });
+  return usuarios;
 }
 
-async function getUsuarios(){
-    usuarios = await dbConnectionWrapper(async (dbCon) => {
-        const result = await dbCon.collection("users").find().toArray()
-        return result
-    })
-    return usuarios;
+async function getUsuarioById(userEmail) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const userFound = await dbCon
+      .collection("users")
+      .findOne({ email: userEmail });
+
+    return userFound;
+  });
+  return result;
 }
 
-async function getUsuarioById(userEmail){
-    
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const userFound = await dbCon.collection("users").findOne({email: userEmail})
-        
-        return userFound
-    })
+async function updateUsuario(userEmail, data) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const updateFields = { $set: data };
+    const result = await dbCon
+      .collection("users")
+      .updateOne({ email: userEmail }, updateFields);
+
     return result;
+  });
+  return result;
 }
 
-async function updateUsuario(userEmail, data){
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const updateFields = { $set: data };
-        const result = await dbCon.collection("users").updateOne({ email: userEmail }, updateFields);
-        
-        return result
-    })
+async function deleteUsuario(userEmail) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const result = await dbCon
+      .collection("users")
+      .deleteOne({ email: userEmail });
+
     return result;
+  });
+  return result;
 }
 
-async function deleteUsuario(userEmail){
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const result = await dbCon.collection("users").deleteOne({ email: userEmail });
-        
-        return result
-    })
+async function createJuego(data) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const result = await dbCon.collection("juegos").insertOne(data);
     return result;
+  });
+  return result;
 }
 
-
-async function createJuego(data){
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const result = await dbCon.collection("juegos").insertOne(data)
-        return result
-    })
+async function getJuegos() {
+  juegos = await dbConnectionWrapper(async (dbCon) => {
+    const result = await dbCon.collection("juegos").find().toArray();
     return result;
+  });
+  return juegos;
 }
 
-async function getJuegos(){
-    juegos = await dbConnectionWrapper(async (dbCon) => {
-        const result = await dbCon.collection("juegos").find().toArray()
-        return result
-    })
-    return juegos;
+async function getJuegoById(juegoId) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const juegoFound = await dbCon
+      .collection("juegos")
+      .findOne({ id: juegoId });
+    return juegoFound;
+  });
+  return result;
 }
 
-async function getJuegoById(juegoId){
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const juegoFound = await dbCon.collection("juegos").findOne({id: juegoId})
-        return juegoFound
-    })
+async function updateJuego(juegoId, data) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const updateFields = { $set: data };
+    const result = await dbCon
+      .collection("juegos")
+      .updateOne({ id: juegoId }, updateFields);
     return result;
+  });
+  return result;
 }
 
-async function updateJuego(juegoId, data){
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const updateFields = { $set: data };
-        const result = await dbCon.collection("juegos").updateOne({ id: juegoId }, updateFields);
-        return result
-    })
+async function deleteJuego(juegoId) {
+  const result = await dbConnectionWrapper(async (dbCon) => {
+    const result = await dbCon.collection("juegos").deleteOne({ id: juegoId });
     return result;
-}
-
-async function deleteJuego(juegoId){
-    result = await dbConnectionWrapper(async (dbCon) => {
-        const result = await dbCon.collection("juegos").deleteOne({ id: juegoId });
-        return result
-    })
-    return result;
+  });
+  return result;
 }
 
 module.exports = {
-        createUsuario,
-        getUsuarios,
-        getUsuarioById,
-        updateUsuario,
-        deleteUsuario,
-        createJuego,
-        getJuegos,
-        getJuegoById,
-        updateJuego,
-        deleteJuego
-    };
+  createUsuario,
+  getUsuarios,
+  getUsuarioById,
+  updateUsuario,
+  deleteUsuario,
+  createJuego,
+  getJuegos,
+  getJuegoById,
+  updateJuego,
+  deleteJuego,
+};
