7e95f4a35b8d5b30d6e5bcc4d13acf7953ae866d
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-23 18:56:14 -0800

    Fixed webpack

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,22 +1,38 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
     entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js'
     },
-    mode: 'development',
+    mode: 'production',
     devtool: 'source-map',
-    target: 'node',
+    target: "node",
+    experiments: {
+        topLevelAwait: true,
+    },
     node: {
-        __dirname: false
+        __dirname: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.node$/,
+                use: ["raw-loader"],
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+        ],
     },
     plugins: [
-        new webpack.EnvironmentPlugin([
-            "NODE_ENV",
-            "SECRET",
-            "PORT",
-        ]),
+        // new webpack.EnvironmentPlugin([
+        //     "NODE_ENV",
+        //     "SECRET",
+        //     "PORT",
+        //     ""
+        // ]),
     ],
 };
