67f3df8ab6077aa6280e7b0ca088d4ad46191bf8
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-24 12:40:29 -0800

    segregated code

--- backend/apis/solanaPay.api.js
+++ backend/apis/solanaPay.api.js
@@ -2,7 +2,7 @@ const juegoService = require("../services/juego.service.js");
 const { Connection, Keypair, PublicKey } = require("@solana/web3.js");
 const { encodeURL, validateTransfer, findReference } = require("@solana/pay");
 const BigNumber = require("bignumber.js");
-const base58 = require("bs58");
+
 // CONSTANTS
 const destinyWallet = process.env.WALLET;
 const recipient = new PublicKey(destinyWallet);
@@ -19,64 +19,60 @@ const paymentRequests = new Map();
 // const keypair = Keypair.fromSecretKey(privateKeyBytes);
 // const publicKey = "9m5TqpsHkPmTYz5aRraLd1ntTtAaLuWuXcMCsRpuvjg8"
 
-const generatePayment = async (req, res) => {
-  if (req.method === "POST") {
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
-    const juego = juegoService.getJuegoById(req.params.id);
-    if (!juego) {
-      return res.status(404).send("Juego not found");
-    }
-
-    const amount = new BigNumber(juego.precio);
-    const message = juego.nombre;
-    const memo = juego.nombre;
-
-    try {
-      const reference = new Keypair().publicKey;
-      const urlData = await generateUrl(
-        recipient,
-        amount,
-        reference,
-        label,
-        message,
-        memo
-      );
-      const ref = reference.toBase58();
-      paymentRequests.set(ref, {
-        recipient,
-        amount,
-        memo,
-      });
-      const { url } = urlData;
-      return res.status(200).send({ url: url.toString(), ref });
-    } catch (error) {
-      return res.status(500).send("Internal Server Error");
-    }
-  } else if (req.method === "GET") {
-    const reference = req.params.reference;
-    if (!reference) {
-      res.status(400).send("Missing reference query parameter");
-      return;
-    }
-
-    try {
-      const referencePublicKey = new PublicKey(reference.toString());
-      const response = await verifyTransaction(referencePublicKey);
-      console.log(response);
-      if (response) {
-        res.status(200).send("Payment verified");
-      } else {
-        res.status(400).send("Payment not verified");
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  } else {
-    res.status(405).send("Method not allowed");
+const verifyPayment = async (req, res) => {
+  const juego = juegoService.getJuegoById(req.params.id);
+  if (!juego) {
+    return res.status(404).send("Juego not found");
   }
+
+  const amount = new BigNumber(juego.precio);
+  const message = juego.nombre;
+  const memo = juego.nombre;
+
+  try {
+    const reference = new Keypair().publicKey;
+    const urlData = await generateUrl(
+      recipient,
+      amount,
+      reference,
+      label,
+      message,
+      memo
+    );
+    const ref = reference.toBase58();
+    paymentRequests.set(ref, {
+      recipient,
+      amount,
+      memo,
+    });
+    const { url } = urlData;
+    return res.status(200).send({ url: url.toString(), ref });
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const generatePayment = async (req, res) => {
+  const reference = req.params.reference;
+  if (!reference) {
+    res.status(400).send("Missing reference query parameter");
+    return;
+  }
+
+  try {
+    const referencePublicKey = new PublicKey(reference.toString());
+    const response = await verifyTransaction(referencePublicKey);
+    console.log(response);
+    if (response) {
+      res.status(200).send("Payment verified");
+    } else {
+      res.status(400).send("Payment not verified");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+  res.status(405).send("Method not allowed");
 };
 
 async function generateUrl(recipient, amount, reference, label, message, memo) {
@@ -114,7 +110,7 @@ const verifyTransaction = async (reference) => {
       amount,
       splToken: undefined,
       reference,
-      // memo
+      memo
     },
     { commitment: "confirmed" }
   );
@@ -126,6 +122,6 @@ const verifyTransaction = async (reference) => {
 };
 
 module.exports = {
-  generateUrl,
+  verifyPayment,
   generatePayment,
 };
--- backend/routers/solana.router.js
+++ backend/routers/solana.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const solanaPay = require('../apis/solanaPay.api.js');
 const multer = require('multer');
 const upload = multer();
-router.get('/:reference', upload.none(), solanaPay.generatePayment);
+router.get('/:reference', upload.none(), solanaPay.verifyPayment);
 router.post('/:juegoId', upload.none(), solanaPay.generatePayment);
 
 module.exports = router;
