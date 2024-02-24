77863b677b7966fc0e1b7b531eaa831aab615dd8
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-23 18:06:46 -0800

    Added multer

--- backend/routers/usuario.router.js
+++ backend/routers/usuario.router.js
@@ -1,14 +1,13 @@
 const userController = require('../controllers/usuario.controller.js');
 const multer = require('multer');
+const upload = multer();
 const express = require('express');
-const bodyParser = require('body-parser');
 const router = express.Router();
-const app = express();
 
-router.get('/',userController.getUsuarios);
-router.post('/login',userController.loginUsuario);
-router.post('/',userController.createUsuario);
-router.put('/:email',userController.updateUsuario);
-router.delete('/',userController.deleteUsuario);
-router.get('/:email',userController.getUsuarioById);
+router.get('/',upload.none(),userController.getUsuarios);
+router.post('/login',upload.none(),userController.loginUsuario);
+router.post('/',upload.none(),userController.createUsuario);
+router.put('/:email',upload.none(),userController.updateUsuario);
+router.delete('/',upload.none(),userController.deleteUsuario);
+router.get('/:email',upload.none(),userController.getUsuarioById);
 module.exports = router;
