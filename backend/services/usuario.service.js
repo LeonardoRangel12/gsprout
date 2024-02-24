722ad62f5feb592c19c2be605628349b6d361d85
antsz25 <l20211845@tectijuana.edu.mx>
2024-02-23 17:41:00 -0800

    Created usuario service

---
+++ backend/services/usuario.service.js
@@ -0,0 +1,24 @@
+const db = require("../configurations/db.configuration.js")
+
+async function createUsuario(data) {
+    
+}
+async function getUsuarios() {
+    
+}
+async function getUsuarioById(email) {
+
+}
+async function updateUsuario(email, data) {
+
+}
+async function deleteUsuario(email) {
+
+}
+module.exports = {
+    createUsuario,
+    getUsuarios,
+    getUsuarioById,
+    updateUsuario,
+    deleteUsuario
+}
