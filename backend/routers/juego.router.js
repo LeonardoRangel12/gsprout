fd94d8b21dcdff18718d80b574dd48a2d45f4046
antsz25 <l20211845@tectijuana.edu.mx>
2024-02-23 18:42:09 -0800

    Router juegos added

---
+++ backend/routers/juego.router.js
@@ -0,0 +1,13 @@
+const juegocontroller = require('../controllers/juego.controller');
+const multer = require('multer');
+const upload = multer();
+const express = require('express');
+const router = express.Router();
+
+router.get('/',upload.none(),juegocontroller.getJuegos);
+router.post('/',upload.none(),juegocontroller.createJuego);
+router.put('/:id',upload.none(),juegocontroller.updateJuego);
+router.delete('/:id',upload.none(),juegocontroller.deleteJuego);
+router.get('/:id',upload.none(),juegocontroller.getJuegoById);
+
+module.exports = router;
