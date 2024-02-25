6c27f6756b83cb56d9052179835db8b3e95b03ec
Luispantoja1102 <l20211821@tectijuana.edu.mx>
2024-02-25 10:39:46 -0800

    WishList

--- frontend/src/components/navbarComponent.vue
+++ frontend/src/components/navbarComponent.vue
@@ -17,11 +17,14 @@
         </ul>
         <!-- Header Icons -->
         <div class="hidden xl:flex items-center space-x-5 items-center">
-          <a class="hover:text-gray-200" href="#">
+          <router-link to="/WishList">
+            <a class="hover:text-gray-200" href="#">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
           </a>
+          </router-link>
+          
           <a class="flex items-center hover:text-gray-200" href="#">
             <span class="flex absolute -mt-5 ml-4">
               <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
