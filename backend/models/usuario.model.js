13f43721a2e1080695c5b81447eb218c30eef174
antsz25 <l20211845@tectijuana.edu.mx>
2024-02-24 17:15:04 -0800

    Modelo de usuario modificado

--- backend/models/usuario.model.js
+++ backend/models/usuario.model.js
@@ -4,6 +4,10 @@ const usuarioSchema = Joi.object({
     username: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(30).required(),
+    nombre: Joi.string().min(3).max(30).required(),
+    apellido: Joi.string().min(3).max(30).required(),
+    descripcion: Joi.string().min(3).max(100).optional(),
+    horas_juego: Joi.number().min(0).optional(),
 });
 
 module.exports = usuarioSchema;
