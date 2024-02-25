784a186d47202e7aeb80ef8058d690a4e5242db1
madeline <madgrismad@gmail.com>
2024-02-25 13:14:52 -0800

    Refactor StoreComponent.vue layout and styling

--- frontend/src/components/StoreComponent.vue
+++ frontend/src/components/StoreComponent.vue
@@ -10,9 +10,12 @@
         <div class="flex items-center space-x-4">
           <div class="flex bg-gray-800 items-center p-2 rounded-md">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+              <path fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd" />
             </svg>
-            <input class="bg-gray-800 outline-none ml-1 block text-white" type="text" name="" id="" placeholder="Buscar...">
+            <input class="bg-gray-800 outline-none ml-1 block text-white" type="text" name="" id=""
+              placeholder="Buscar...">
           </div>
           <div class="space-x-4">
             <button class="bg-indigo-600 px-4 py-2 rounded-md font-semibold cursor-pointer">Nuevo Reporte</button>
@@ -22,65 +25,73 @@
       </div>
       <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-<table class="min-w-full leading-normal">
-<thead>
-<tr>
-<th class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-Nombre
-</th>
-<th class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-Descripción
-</th>
-<th class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-Precio
-</th>
-<th class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-Vendedor
-</th>
-<th class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-Tiempo Restante
-</th>
-<th class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-Estado
-</th>
-</tr>
-</thead>
-<tbody>
-<tr v-for="juego in juegos" :key="juego.id" :class="{ 'bg-gray-700': juego.activo, 'bg-gray-900': !juego.activo }">
-<td class="px-5 py-5 border-b border-gray-800 text-sm">
-<div class="flex items-center">
-<div class="flex-shrink-0 w-10 h-10">
-<img class="w-full h-full rounded-full" :src="juego.imagen" :alt="juego.nombre" />
-</div>
-<div class="ml-3">
-<p class="text-gray-300 whitespace-no-wrap">{{ juego.nombre }}</p>
-</div>
-</div>
-</td>
-<td class="px-5 py-5 border-b border-gray-800 text-sm">
-<p class="text-gray-300 whitespace-no-wrap">{{ juego.descripcion }}</p>
-</td>
-<td class="px-5 py-5 border-b border-gray-800 text-sm">
-<p class="text-gray-300 whitespace-no-wrap">{{ juego.precio }} SOL</p>
-<p class="text-gray-300 whitespace-no-wrap">{{ (juego.precio * SOL_TO_USD_RATE).toFixed(2) }} USD</p>
-</td>
-<td class="px-5 py-5 border-b border-gray-800 text-sm">
-<p class="text-gray-300 whitespace-no-wrap">{{ juego.vendedor || 'gsprout' }}</p>
-</td>
-<td class="px-5 py-5 border-b border-gray-800 text-sm">
-<p class="text-gray-300 whitespace-no-wrap">{{ juego.remainingTime }}</p>
-</td>
-<td class="px-5 py-5 border-b border-gray-800 text-sm">
-<span v-if="juego.activo" class="px-3 py-1 font-semibold text-green-900 bg-green-200 rounded-full">Activo</span>
-<span v-else class="px-3 py-1 font-semibold text-red-900 bg-red-200 rounded-full">Inactivo</span>
-</td>
-</tr>
-</tbody>
-</table>
-</div>
-</div>
-</div>
-<Footer />
+          <table class="min-w-full leading-normal">
+            <thead>
+              <tr>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
+                  Nombre
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
+                  Descripción
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
+                  Precio
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
+                  Vendedor
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
+                  Tiempo Restante
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
+                  Estado
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="juego in juegos" :key="juego.id"
+                :class="{ 'bg-gray-700': juego.activo, 'bg-gray-900': !juego.activo }">
+                <td class="px-5 py-5 border-b border-gray-800 text-sm">
+                  <div class="flex items-center">
+                    <div class="flex-shrink-0 w-10 h-10">
+                      <img class="w-full h-full rounded-full" :src="juego.imagen" :alt="juego.nombre" />
+                    </div>
+                    <div class="ml-3">
+                      <p class="text-gray-300 whitespace-no-wrap">{{ juego.nombre }}</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-800 text-sm">
+                  <p class="text-gray-300 whitespace-no-wrap">{{ juego.descripcion }}</p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-800 text-sm">
+                  <p class="text-gray-300 whitespace-no-wrap">{{ juego.precio }} SOL</p>
+                  <p class="text-gray-300 whitespace-no-wrap">{{ (juego.precio * SOL_TO_USD_RATE).toFixed(2) }} USD</p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-800 text-sm">
+                  <p class="text-gray-300 whitespace-no-wrap">{{ juego.vendedor || 'gsprout' }}</p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-800 text-sm">
+                  <p class="text-gray-300 whitespace-no-wrap">{{ juego.remainingTime }}</p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-800 text-sm">
+                  <span v-if="juego.activo"
+                    class="px-3 py-1 font-semibold text-green-900 bg-green-200 rounded-full">Activo</span>
+                  <span v-else class="px-3 py-1 font-semibold text-red-900 bg-red-200 rounded-full">Inactivo</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <Footer />
 
   </div>
 </template>
@@ -137,6 +148,4 @@ export default {
   }
 };
 </script>
-<style scoped>
-/* Estilos específicos para este componente van aquí */
-</style>
+<style scoped>/* Estilos específicos para este componente van aquí */</style>
