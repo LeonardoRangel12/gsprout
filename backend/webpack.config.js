e40dcb97f79ce557dfcb06423b07b9d5951d2727
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-25 08:52:23 -0800

    Testing

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         filename: 'app.bundle.js'
     },
     mode: 'production',
-    devtool: 'source-map',
+    // devtool: 'source-map',
     target: "node",
     experiments: {
         topLevelAwait: true,
