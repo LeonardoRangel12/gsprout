10f3360c8a9a1729da674ae75e07b778271b6e4e
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-03-16 16:31:20 -0700

    Fixed issue with usuario model

--- backend/models/usuario.model.js
+++ backend/models/usuario.model.js
@@ -6,7 +6,7 @@ const usuarioSchema = Joi.object({
     // password: Joi.string().required(),
     password: Joi.string().min(8).max(30).required(),
     wallets: Joi.array().items(Joi.string().min(44).max(44)).min(1).required(),
-    watchList: Joi.array().items(Joi.string().min(3).max(30)).default([]).required(),
+    watchList: Joi.array().items(Joi.string().min(3).max(30)).default([]).optional(),
     descripcion: Joi.string().min(3).max(100).optional(),
     // horas_juego: Joi.number().min(0).optional(),
     // juego_favorito: Joi.string().min(3).max(30).optional(),
