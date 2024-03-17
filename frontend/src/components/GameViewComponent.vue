fb2f238dac7e820365a345cf3f6ac12fda5b9afa
Luispantoja1102 <l20211821@tectijuana.edu.mx>
2024-03-17 10:34:46 -0700

    Modif game view component V1

--- frontend/src/components/GameViewComponent.vue
+++ frontend/src/components/GameViewComponent.vue
@@ -5,22 +5,35 @@
             <div class="grid grid-cols-1 sm:grid-cols-20 gap-6 px-4">
                 <div class="col-span-1 sm:col-span-9">
                     <div class="bg-gray-800 rounded-lg p-6 shadow-lg">
-
-                        <div class="bg-gray-200 rounded-lg p-6 shadow-lg">
-                        <img src="https://imgur.com/5O9qO7w" class="w-full rounded-md">
-                        <div class="flex justify-between items-center mt-4">
-                            <span class="text-lg font-bold">$19.99</span> 
-                            <button class="bg-blue-500 text-white px-3 py-2 rounded-md">Comprar</button>
-                        </div>
-                    </div>
-                        <h2 class="text-2xl font-bold mb-4 text-white">Name Game</h2>
-                        <h2 class="text-2xl font-bold mb-4 text-white">Descripcion</h2>
-                        <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est
-                            vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas
-                            suscipit. Nunc finibus vel ante id euismod. Vestibulum ante ipsum primis in faucibus orci luctus
-                            et ultrices posuere cubilia Curae; Aliquam erat volutpat. Nulla vulputate pharetra tellus, in
-                            luctus risus rhoncus id.
-                        </p>
+                        <div class="flex flex-col bg-gray-700 rounded-lg p-4 shadow-lg md:flex-row">
+                            <div class="grid gap-4 mb-4 md:mb-0 md:mr-4">
+                                <div>
+                                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg" alt="">
+                                </div>
+                                <div class="grid grid-cols-5 gap-4">
+                                <div>
+                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+                                </div>
+                                <div>
+                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+                                </div>
+                                <div>
+                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+                                </div>
+                                <div>
+                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
+                                </div>
+                                <div>
+                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
+                                </div>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 class="text-2xl font-bold mb-4 text-white">Name Game</h2>
+                                <h2 class="text-2xl font-bold mb-4 text-white">Descripcion</h2>
+                                <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit. Nunc finibus vel ante id euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Nulla vulputate pharetra tellus, in luctus risus rhoncus id. </p>
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div>
