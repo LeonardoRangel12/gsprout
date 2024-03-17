881c427701f3d8b02412b2100261c45b0faba0ea
madeline <madgrismad@gmail.com>
2024-03-16 21:25:48 -0700

    Actualizar componentes HeroComponent.vue y StoreComponent.vue***
    ***Actualizar los componentes HeroComponent.vue y StoreComponent.vue

--- frontend/src/components/StoreComponent.vue
+++ frontend/src/components/StoreComponent.vue
@@ -8,66 +8,27 @@
           <span class="text-xs">Todos los juegos disponibles</span>
         </div>
       </div>
-      <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-        <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-          <table class="min-w-full leading-normal">
-            <thead>
-              <tr>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-                  Nombre
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-                  Precio
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-                  Vendedor
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-                  
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-800 bg-gray-800 text-left text-xs font-semibold uppercase tracking-wider">
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="juego in juegos" :key="juego.id"
-                :class="{ 'bg-gray-700': juego.activo, 'bg-gray-900': !juego.activo }">
-                <td class="px-5 py-5 border-b border-gray-800 text-sm">
-                  <div class="flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10">
-                      <img class="w-full h-full rounded-full" :src="juego.imagen" :alt="juego.nombre" />
-                    </div>
-                    <div class="ml-3">
-                      <p class="text-gray-300 whitespace-no-wrap">{{ juego.nombre }}</p>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-800 text-sm">
-                  <p class="text-gray-300 whitespace-no-wrap">{{ juego.precio }} SOL</p>
-                  <p class="text-gray-300 whitespace-no-wrap">{{ (juego.precio * SOL_TO_USD_RATE).toFixed(2) }} USD</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-800 text-sm">
-                  <p class="text-gray-300 whitespace-no-wrap">{{ juego.vendedor || 'gsprout' }}</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-800 text-sm">
-                  <button type="submit" @click="switchToBuy(juego._id)"
-                    class="py-1 px-2 bg-indigo-700 text-white font-bold rounded hover:bg-indigo-500">Comprar</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div v-for="juego in juegos" :key="juego.id" class="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+          <img class="w-full h-56 object-cover object-center" :src="juego.imagen" :alt="juego.nombre" />
+          <div class="p-4">
+            <h3 class="text-lg font-semibold">{{ juego.nombre }}</h3>
+            <p class="text-gray-300 mb-4">{{ juego.descripcion }}</p>
+            <div class="flex justify-between items-center">
+              <div>
+                <p class="text-gray-300 mb-2">{{ juego.precio }} SOL</p>
+                <p class="text-gray-300">{{ (juego.precio * SOL_TO_USD_RATE).toFixed(2) }} USD</p>
+              </div>
+              <button @click="switchToBuy(juego._id)" class="px-3 py-1 bg-indigo-700 text-white font-semibold rounded hover:bg-indigo-500">Comprar</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <Footer />
-
   </div>
 </template>
+
 <script>
 import Navbar from './navbarComponent.vue';
 import Footer from './FooterComponent.vue';
@@ -116,6 +77,7 @@ export default {
   }
 };
 </script>
+
 <style scoped>
 /* Estilos específicos para este componente van aquí */
 </style>
