a61fdbcdadb096e9aa611ffbf09ce4ee421ee85b
madeline <madgrismad@gmail.com>
2024-03-16 20:24:38 -0700

    Actualizar estilos de botones en DiscordComponent.vue

--- frontend/src/components/DiscordComponent.vue
+++ frontend/src/components/DiscordComponent.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="relative w-160 h-90 mx-auto overflow-hidden">
+    <div class="relative w-96 h-54 mx-auto overflow-hidden">
       <div class="flex transition-transform duration-500" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
-        <img v-for="game in newGames" :key="game._id" :src="game.imagen" :alt="game.nombre + ' Image'" class="w-160 h-90 object-cover" />
+        <img v-for="game in newGames" :key="game._id" :src="game.imagen" :alt="game.nombre + ' Image'" class="w-full h-full object-cover aspect-w-16 aspect-h-9" />
       </div>
-      <button @click="prev" class="absolute top-1/2 left-0 transform -translate-y-1/2 ml-2 bg-gray-800 text-white rounded-full w-8 h-8 flex items-center justify-center text-xl">&#8249;</button>
-      <button @click="next" class="absolute top-1/2 right-0 transform -translate-y-1/2 mr-2 bg-gray-800 text-white rounded-full w-8 h-8 flex items-center justify-center text-xl">&#8250;</button>
+      <button @click="prev" class="absolute top-1/2 left-0 transform -translate-y-1/2 ml-2 bg-gray-800 text-white rounded-full w-6 h-6 flex items-center justify-center text-sm">&#8249;</button>
+      <button @click="next" class="absolute top-1/2 right-0 transform -translate-y-1/2 mr-2 bg-gray-800 text-white rounded-full w-6 h-6 flex items-center justify-center text-sm">&#8250;</button>
     </div>
   </template>
   
