ddfa30662f4316ed1aeeedafa102dad0af92bf84
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-03-31 21:36:32 -0700

    Updated redis in production

--- backend/configurations/redis.configuration.js
+++ backend/configurations/redis.configuration.js
@@ -1,30 +1,20 @@
   if (process.env.NODE_ENV === "production") {
     const { createClient } = require("redis");
-    const connectToRedis= async()=>{
-      const client = createClient({
-        url: "redis://127.0.0.1:6379"
+    createClient({
+      url: 'redis://127.0.0.1:6379'
+    })
+      .on("error", (err) => {
+        console.log("Redis Error: " + err);
       })
-
-      client.on("error", (error) => {
-        console.error(error);
-      });
-
-      await client.connect();
-      console.log("Connected to Redis");
-      return client;
-    }
-    module.exports = connectToRedis;
+      .connect().then((client) => {
+        module.exports = client;});
 
   }
 
   if (process.env.NODE_ENV === "development") {
-    const connectToRedis = async () => {
-      const { createClient } = require("redis-mock");
-      const client = createClient();
-      // Polyfill for setEx
-      client.setEx = client.setex;
-      return client;
-    }
-    module.exports = connectToRedis;
-
+    const { createClient } = require("redis-mock");
+    const client = createClient();
+    // Polyfill for setEx
+    client.setEx = client.setex;
+    module.exports = client;
   }
--- backend/utils/redis.util.js
+++ backend/utils/redis.util.js
@@ -1,8 +1,7 @@
-const connectToRedis = require("../configurations/redis.configuration");
+const redisConnection = require("../configurations/redis.configuration");
 const getCache = async (key) => {
   // const redisKey = await cryptojsUtil.hashText(key);
   const redisKey = key;
-  const redisConnection = await connectToRedis();
   const res = await new Promise((resolve, reject) => {
     redisConnection.get(redisKey, (err, data) => {
       if (err) reject(err);
@@ -14,13 +13,11 @@ const getCache = async (key) => {
 
 const setCache = async (key, data, expiration = 3600) => {
   // const redisKey = await cryptojsUtil.hashText(key);
-  const redisConnection = await connectToRedis();
   const redisKey = key;
   redisConnection.setEx(redisKey, expiration, JSON.stringify(data));
 };
 
 const deleteCache = async (key) => {
-  const redisConnection = await connectToRedis();
   // const redisKey = await cryptojsUtil.hashText(key);
   const redisKey = key;
   redisConnection.del(redisKey);
