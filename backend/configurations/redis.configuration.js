70ab6a2fca2b06d77203e43a2f7a7d5ee5557c39
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-03-31 21:52:44 -0700

    Changed to redis mock

--- backend/configurations/redis.configuration.js
+++ backend/configurations/redis.configuration.js
@@ -1,14 +1,18 @@
   if (process.env.NODE_ENV === "production") {
-    const { createClient } = require("redis");
-    createClient({
-      url: 'redis://127.0.0.1:6379'
-    })
-      .on("error", (err) => {
-        console.log("Redis Error: " + err);
-      })
-      .connect().then((client) => {
-        module.exports = client;});
-
+    // const { createClient } = require("redis");
+    // createClient({
+    //   url: 'redis://127.0.0.1:6379'
+    // })
+    //   .on("error", (err) => {
+    //     console.log("Redis Error: " + err);
+    //   })
+    //   .connect().then((client) => {
+    //     module.exports = client;});
+    const { createClient } = require("redis-mock");
+    const client = createClient();
+    // Polyfill for setEx
+    client.setEx = client.setex;
+    module.exports = client;
   }
 
   if (process.env.NODE_ENV === "development") {
