3de3b4aae346ae865aef125db6a5c5d4a121efc4
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-03-31 21:51:28 -0700

    Added cache again

--- backend/routers/publicaciones.router.js
+++ backend/routers/publicaciones.router.js
@@ -5,10 +5,10 @@ const express = require('express');
 const { handleCache } = require('../middleware/redis.middleware');
 const router = express.Router();
 
-router.get('/',upload.none(), /*handleCache,*/controller.getPublicaciones);  
-router.get('/:id',upload.none(), /*handleCache,*/controller.getPublicacionById, /*handleCache*/);
-router.post('/',upload.none(),controller.createPublicacion, /*handleCache*/);
-router.put('/:id',upload.none(),controller.updatePublicacion, /*handleCache*/);
-router.delete('/:id',upload.none(),controller.deletePublicacion, /*handleCache*/);
+router.get('/',upload.none(), handleCache,controller.getPublicaciones);  
+router.get('/:id',upload.none(), handleCache,controller.getPublicacionById, handleCache);
+router.post('/',upload.none(),controller.createPublicacion, handleCache);
+router.put('/:id',upload.none(),controller.updatePublicacion, handleCache);
+router.delete('/:id',upload.none(),controller.deletePublicacion, handleCache);
 
 module.exports = router;
