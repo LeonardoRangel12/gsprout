e5c6b0580abad2ae85f34acc705cf73eadc9ad47
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-04-01 23:47:05 -0700

    Updated exchange api key

--- backend/utils/exchange.util.js
+++ backend/utils/exchange.util.js
@@ -4,7 +4,7 @@ const getExchange = async (from = "USDT", to = "SOL") => {
         method: 'GET',
         url: `https://crypto-and-forex-rates.p.rapidapi.com/api/exchange/${from}/${to}/1/1/1/3`,
         headers: {
-          'X-RapidAPI-Key': '76a699302amsh2331ae6790adb61p1224cfjsnb665e672f660',
+          'X-RapidAPI-Key': '039c2fc7e8msh483a1607d2f72b7p19a785jsn2311317d30ee',
           'X-RapidAPI-Host': 'crypto-and-forex-rates.p.rapidapi.com'
         }
     };
