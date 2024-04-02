04290dd17d7d99e5ddd5923a8a5c3b8116f49c85
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-04-02 00:07:08 -0700

    Added timeout to exchange api

--- backend/utils/exchange.util.js
+++ backend/utils/exchange.util.js
@@ -6,14 +6,16 @@ const getExchange = async (from = "USDT", to = "SOL") => {
         headers: {
           'X-RapidAPI-Key': '039c2fc7e8msh483a1607d2f72b7p19a785jsn2311317d30ee',
           'X-RapidAPI-Host': 'crypto-and-forex-rates.p.rapidapi.com'
-        }
+        },
+        timeout: 5000
     };
     try {
         const response = await axios.request(options);
         return response.data;
     } catch (error) {
         console.error(error);
-        return null;
+        // return null;
+        return 200;
     }
 }
 module.exports = {getExchange};
